b241261b53e28e80851e93455175c04b7d161762
Evan McKee <emckee@gmail.com>
2015-10-23 17:33:14 -0700

    Initial Commit

---
+++ IntensityAttributingKit/IntensityAttributingKit.h
@@ -0,0 +1,19 @@
+//
+//  IntensityAttributingKit.h
+//  IntensityAttributingKit
+//
+//  Created by Evan Mckee on 10/23/15.
+//  Copyright © 2015 McKeeMaKer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IntensityAttributingKit.
+FOUNDATION_EXPORT double IntensityAttributingKitVersionNumber;
+
+//! Project version string for IntensityAttributingKit.
+FOUNDATION_EXPORT const unsigned char IntensityAttributingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IntensityAttributingKit/PublicHeader.h>
+
+
