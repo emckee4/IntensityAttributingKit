7f8b4777ef70b685b26809d56ec653c0b6f25790
Evan McKee <emckee@gmail.com>
2015-11-05 12:19:05 -0800

    PressureButton background now varies based on intensity of current press

--- IntensityAttributingKit/PressureButton.swift
+++ IntensityAttributingKit/PressureButton.swift
@@ -27,21 +27,46 @@ class PressureButton: UIButton {
         
     }
     
-    var peakPressure:CGFloat {
-        return pressureHistory.maxElement() ?? 0.0
+    private var baseBackgroundColor:UIColor? {
+        didSet {super.backgroundColor = baseBackgroundColor}
     }
+    override var backgroundColor:UIColor? {
+        get{return baseBackgroundColor }
+        set{baseBackgroundColor = newValue}
+    }
+
+    
+    private func setBackgroundColorForIntensity(intensityVal:CGFloat){
+        guard baseBackgroundColor != nil else {return}
+        guard intensityVal > 0.0 else {super.backgroundColor = baseBackgroundColor; return}
+        let boundedIntensity = bound(intensityVal, min: 0.0, max: 1.0)
+        var white:CGFloat = -1.0
+        var alpha:CGFloat = 1.0
+        baseBackgroundColor!.getWhite(&white, alpha: &alpha)
+        
+        let newAlpha:CGFloat = max(alpha * CGFloat(1 + boundedIntensity), 1.0)
+        let newWhite:CGFloat = white * CGFloat(1 - boundedIntensity)
+        super.backgroundColor = UIColor(white: newWhite, alpha: newAlpha)
+    }
+    
+    
+//    var peakPressure:CGFloat {
+//        return pressureHistory.maxElement() ?? 0.0
+//    }
     
     
     override func beginTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
         pressureHistory = []
         if touchInside {
             pressureHistory.append(pressure(touch))
+            setBackgroundColorForIntensity(pressure(touch))
         }
         return super.beginTrackingWithTouch(touch, withEvent: event)
     }
     override func continueTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
         if touchInside {
             pressureHistory.append(pressure(touch))
+            setBackgroundColorForIntensity(pressure(touch))
         }
         return super.continueTrackingWithTouch(touch, withEvent: event)
     }
@@ -49,12 +74,13 @@ class PressureButton: UIButton {
         if touchInside && touch != nil {
             pressureHistory.append(pressure(touch!))
         }
-        
+        setBackgroundColorForIntensity(0.0)
         super.endTrackingWithTouch(touch, withEvent: event)
         
     }
     override func cancelTrackingWithEvent(event: UIEvent?) {
         super.cancelTrackingWithEvent(event)
+        setBackgroundColorForIntensity(0.0)
     }
     
     
