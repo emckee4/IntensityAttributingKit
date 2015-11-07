a8e8cd01201f4bfc5a0e1e793f0b96e62618af4a
Evan McKee <emckee@gmail.com>
2015-11-07 13:00:52 -0800

    Duration based intensity measurements now properly supported, as RawIntensity sets NSDates on init/reset. ForceIntensityMappingFunctions separated into its own file. The Duration embedded struct carries a static variable timeToFull which is used by the new Duration.linearScaleToConstant function to determine intensity.

---
+++ IntensityAttributingKit/ForceIntensityMappingFunctions.swift
@@ -0,0 +1,72 @@
+//
+//  ForceIntensityMappingFunctions.swift
+//  IntensityAttributingKit
+//
+//  Created by Evan Mckee on 11/7/15.
+//  Copyright © 2015 McKeeMaKer. All rights reserved.
+//
+
+import Foundation
+
+
+public struct ForceIntensityMappingFunctions {
+    
+    public struct Linear {
+        ///returns max(forceHistory) /
+        public static func maxForce(raw:RawIntensity)->Float{
+            return (raw.forceHistory.maxElement() ?? 0.0) / raw.maximumPossibleForce
+        }
+        
+        ///average not including first value
+        public static func averageForce(raw:RawIntensity)->Float{
+            let count = raw.forceHistory.count
+            return (raw.forceHistory[1..<count].reduce(0.0, combine: +) / Float(count - 1)) / raw.maximumPossibleForce
+        }
+        
+        ///Average discarding first value. If the history is of length greater than ten then this only uses the ten most recent values. The interval over which 10 sequential touch events (within the same touch) occur seems to typically work out to a little less than 0.15 seconds in testing.
+        public static func averageLastTen(raw:RawIntensity)->Float{
+            let count = raw.forceHistory.count
+            guard count > 1 else {return 0.0}
+            if count < 10 {
+                return (raw.forceHistory[1..<count].reduce(0.0, combine: +) / Float(count - 1)) / raw.maximumPossibleForce
+            } else {
+                return (raw.forceHistory[(count - 10)..<count].reduce(0.0, combine: +) / Float(10)) / raw.maximumPossibleForce
+            }
+        }
+        
+        ///Similar to averageLastTen except it removes the high and low values in the last 10
+        public static func smoothedAverageLastTen(raw:RawIntensity)->Float{
+            let count = raw.forceHistory.count
+            guard count > 2 else {return raw.forceHistory.maxElement() ?? 0.0}
+            var truncated:[Float]!
+            if count < 10 {
+                truncated = Array<Float>(raw.forceHistory[1..<count])
+            } else {
+                truncated = Array<Float>(raw.forceHistory[(count - 10)..<count])
+            }
+            
+            truncated.sortInPlace()
+            truncated.removeLast()
+            truncated.removeFirst()
+            
+            return (truncated.reduce(0.0, combine: +) / Float(truncated.count)) / raw.maximumPossibleForce
+        }
+        
+        
+        
+    }
+    public struct Duration {
+        public static var timeToFull:NSTimeInterval = 0.5
+        
+        public static func eventCount(raw:RawIntensity)->Float{
+            return min(Float(raw.forceHistory.count) / 100.0, 1.0)
+        }
+        
+        public static func linearScaleToConstant(raw:RawIntensity)->Float{
+            guard raw.startTime != nil && Duration.timeToFull != 0.0 else {return 0.0}
+            return min( Float(NSDate().timeIntervalSinceDate(raw.startTime) / Duration.timeToFull)  , 1.0)
+        }
+        
+        
+    }
+}
--- IntensityAttributingKit/RawIntensity.swift
+++ IntensityAttributingKit/RawIntensity.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 
-struct RawIntensity {
+public struct RawIntensity {
     ///raw force value
     var forceHistory:[Float]
     ///As far as I can tell this is constant on a device and may well be 6.667 across all devices.
@@ -19,7 +19,7 @@ struct RawIntensity {
     var intensity:Float {
         return RawIntensity.forceIntensityMapping(raw: self)
     }
-    
+    var startTime:NSDate!
     
     var avgPressure:Float {
         let count = forceHistory.count
@@ -38,6 +38,7 @@ struct RawIntensity {
     
     mutating func reset(withValue:CGFloat = 0.0){
         forceHistory = [Float(withValue)]
+        startTime = NSDate()
     }
     
     mutating func append(value:CGFloat){
@@ -50,11 +51,13 @@ struct RawIntensity {
     init(withValue:CGFloat, maximumPossibleForce:CGFloat = RawIntensity.maximumPossibleForceForDevice){
         forceHistory = [Float(withValue)]
         self.maximumPossibleForce = Float(maximumPossibleForce)
+        startTime = NSDate()
     }
     
     init(withFloatValue float:Float = 0.0, maximumPossibleForce:Float = Float(RawIntensity.maximumPossibleForceForDevice)){
         forceHistory = [float]
         self.maximumPossibleForce = maximumPossibleForce
+        startTime = NSDate()
     }
     
     ///As far as I can tell maximumPossibleForce is constant across the device
@@ -62,74 +65,13 @@ struct RawIntensity {
     
     
     ///Global mapping function between Force and intensity value
-    static var forceIntensityMapping:(raw:RawIntensity)->Float = ForceIntensityMappingFunctions.Linear.smoothedAverageLastTen
+    public static var forceIntensityMapping:(raw:RawIntensity)->Float = ForceIntensityMappingFunctions.Linear.smoothedAverageLastTen
     
 
-    
-
-    
-    
-    
 }
 
 
-struct ForceIntensityMappingFunctions {
-    
-    struct Linear {
-        ///returns max(forceHistory) /
-        static func maxForce(raw:RawIntensity)->Float{
-            return (raw.forceHistory.maxElement() ?? 0.0) / raw.maximumPossibleForce
-        }
-        
-        ///average not including first value
-        static func averageForce(raw:RawIntensity)->Float{
-            let count = raw.forceHistory.count
-            return (raw.forceHistory[1..<count].reduce(0.0, combine: +) / Float(count - 1)) / raw.maximumPossibleForce
-        }
-        
-        ///Average discarding first value. If the history is of length greater than ten then this only uses the ten most recent values. The interval over which 10 sequential touch events (within the same touch) occur seems to typically work out to a little less than 0.15 seconds in testing.
-        static func averageLastTen(raw:RawIntensity)->Float{
-            let count = raw.forceHistory.count
-            guard count > 1 else {return 0.0}
-            if count < 10 {
-                return (raw.forceHistory[1..<count].reduce(0.0, combine: +) / Float(count - 1)) / raw.maximumPossibleForce
-            } else {
-                return (raw.forceHistory[(count - 10)..<count].reduce(0.0, combine: +) / Float(10)) / raw.maximumPossibleForce
-            }
-        }
-        
-        ///Similar to averageLastTen except it removes the high and low values in the last 10
-        static func smoothedAverageLastTen(raw:RawIntensity)->Float{
-            let count = raw.forceHistory.count
-            guard count > 2 else {return raw.forceHistory.maxElement() ?? 0.0}
-            var truncated:[Float]!
-            if count < 10 {
-                truncated = Array<Float>(raw.forceHistory[1..<count])
-            } else {
-                truncated = Array<Float>(raw.forceHistory[(count - 10)..<count])
-            }
-            
-            truncated.sortInPlace()
-            truncated.removeLast()
-            truncated.removeFirst()
-            
-            return (truncated.reduce(0.0, combine: +) / Float(truncated.count)) / raw.maximumPossibleForce
-        }
 
-        
-        
-    }
-    struct Duration {
-        static func eventCount(raw:RawIntensity)->Float{
-            return min(Float(raw.forceHistory.count) / 100.0, 1.0)
-        }
-        static func eventCountFast2(raw:RawIntensity)->Float{
-            return min(Float(raw.forceHistory.count) / 50.0, 1.0)
-        }
-
-
-    }
-}
 
 
 
