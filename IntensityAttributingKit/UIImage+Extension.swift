ba69cb47f3bf118f777ccc77f20ee5837982c3d0
Evan McKee <emckee@gmail.com>
2015-11-10 11:05:01 -0800

    Refactored UIImage extension replacing prior functions with resizeImageToFit(maxSize maxSize:CGSize, maintainAspectRatio:Bool = true)->UIImage

--- IntensityAttributingKit/UIImage+Extension.swift
+++ IntensityAttributingKit/UIImage+Extension.swift
@@ -10,28 +10,24 @@ import UIKit
 
 
 extension UIImage {
-    ///returns a scaled version of the image such that both the width and height are within the provided parameters while maintaining the aspect ratio
-    //    func resizeWithMaxWidthAndHeight(maxWidth maxWidth:CGFloat,maxHeight:CGFloat)->UIImage{
-    //        let widthScaleFactor = maxWidth / self.size.width
-    //        let heightScaleFactor = maxHeight / self.size.height
-    //        if widthScaleFactor >= 1.0 && heightScaleFactor >= 1.0 {
-    //            return self
-    //        } else {
-    //            let newScale = min(widthScaleFactor,heightScaleFactor)
-    //
-    //            let size = CGSizeApplyAffineTransform(self.size, CGAffineTransformMakeScale(newScale, newScale))
-    //            let hasAlpha = false
-    //            let scale: CGFloat = 0.0 // Automatically use scale factor of main screen
-    //
-    //            UIGraphicsBeginImageContextWithOptions(size, !hasAlpha, scale)
-    //            self.drawInRect(CGRect(origin: CGPointZero, size: size))
-    //
-    //            let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
-    //            UIGraphicsEndImageContext()
-    //            return scaledImage
-    //        }
-    //
-    //    }
+    ///Resizes the image such that it will fit within the maxSize provided. This will not expand the images bounds. By default this will maintain the aspect ratio of the image.
+    func resizeImageToFit(maxSize maxSize:CGSize, maintainAspectRatio:Bool = true)->UIImage{
+        var finalSize:CGSize!
+        if maintainAspectRatio == false {
+            finalSize = CGSize(width: min(self.size.width , maxSize.width ), height: min(self.size.height , maxSize.height ))
+        } else {
+            finalSize = self.size.sizeThatFitsMaintainingAspect(containerSize: maxSize)
+        }
+        let hasAlpha = false
+        let scale: CGFloat = 0.0 // Automatically use scale factor of main screen
+        
+        UIGraphicsBeginImageContextWithOptions(finalSize, !hasAlpha, scale)
+        self.drawInRect(CGRect(origin: CGPointZero, size: finalSize))
+        
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return scaledImage
+    }
     
     ///returns a scaled version of the image such that both the width and height are within the provided parameters while maintaining the aspect ratio.
     func constrainImageBounds(maxWidth maxWidth:CGFloat, maxHeight:CGFloat)->UIImage {
