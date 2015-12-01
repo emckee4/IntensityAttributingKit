aeec8f78ed212b83f7ea6dfe027daf8bf85abb56
Evan McKee <emckee@gmail.com>
2015-12-01 15:36:18 -0800

    tweaked IntensityTransformers

--- IntensityAttributingKit/IntensityTransformers.swift
+++ IntensityAttributingKit/IntensityTransformers.swift
@@ -11,13 +11,14 @@ public enum IntensityTransformers:String {
         if let thisTransformer = IntensityTransformers.storedTransformers[self] {
             return thisTransformer
         } else {
+            ///this isn't really a threadsafe way of initializing the transformers but it shouldn't crash or create a real memory issue either
             let trans = IntensityTransformers.transformerTypes[self]!.init()
             IntensityTransformers.storedTransformers[self] = trans
             return trans
         }
     }
     ///
-    private static var transformerTypes:[IntensityTransformers:IntensityTransforming.Type] = [.HueGYRScheme:HueGYRIntensityScheme.self, .WeightScheme:WeightIntensityScheme.self, FontSizeScheme:FontSizeIntensityScheme.self]
+    private static let transformerTypes:[IntensityTransformers:IntensityTransforming.Type] = [.HueGYRScheme:HueGYRIntensityScheme.self, .WeightScheme:WeightIntensityScheme.self, FontSizeScheme:FontSizeIntensityScheme.self]
     ///Instances of transformers are lazily added to this array as they're requested and instantiated using transformerTypes
     private static var storedTransformers:[IntensityTransformers:IntensityTransforming] = [:]
     
